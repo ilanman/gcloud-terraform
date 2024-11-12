370a04f5196d60d7438c2a179e7560b61587026c
Ilan Man (he/him) <ilan.man@bdmacmb-lvtkgyqqtn.lan>
2024-11-11 19:47:43 -0500

    removing defaults

--- variables.tf
+++ variables.tf
@@ -3,13 +3,11 @@
 variable "project_id" {
   description = "The Google Cloud project ID"
   type        = string
-  default     = "trading-algo-bucket-1234" # setting the default project-id. Can remove this and enter each time.
 }
 
 variable "region" {
   description = "The region for the resources"
   type        = string
-  default     = "us-east4"  # Default region
 }
 
 variable "service_account_email" {
